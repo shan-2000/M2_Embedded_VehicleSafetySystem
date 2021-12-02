c293f6fea9c3064adce41f2e1d787fd57d18f342
shan-2000 <45603597+shan-2000@users.noreply.github.com>
2021-12-02 18:53:27 +0530

    adc.c added

---
+++ 3_Implementation/src/adc.c
@@ -0,0 +1,23 @@
+#include "adc.h"
+
+void InitADC()
+{
+    /*AVcc = +5V = Vref*/
+    ADMUX = (1<<REFS0);
+    /* Enable ADC and set Prescalr to 2 */
+    ADCSRA=(1<<ADEN)|(7<<ADPS0);
+   
+}
+
+
+
+uint16_t ReadADC(uint8_t ch)
+{
+     /*Select ADC Channel: must be 0-7*/
+    ADMUX = (ADMUX & 0xF0) | (ch & 0x0F);
+ //single conversion mode
+    ADCSRA |= (1<<ADSC);
+ // wait until ADC conversion is complete
+    while( ADCSRA & (1<<ADSC) );
+     return ADC;
+}
